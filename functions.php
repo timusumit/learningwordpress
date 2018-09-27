96caca9cd4f49fb0d4134312f1618cb608d2d5ea
Sumit Maharjan <timusumit@gmail.com>
2018-09-27 20:47:40 +0545

    lesson 2 completed

--- functions.php
+++ functions.php
@@ -5,4 +5,15 @@ wp_enqueue_style('style', get_stylesheet_uri());
 
 
 add_action('wp_enqueue_scripts','learningwordpress_resources');
+
+//navigation menus
+
+register_nav_menus(array(
+'primary'=> __('Primary Menu'),
+'footer'=> __('Footer Menu'),
+
+))
+
+
+
 ?>
---
+++ page.php
@@ -0,0 +1,17 @@
+<?php 
+get_header();
+if (have_posts()):
+	while(have_posts()):the_post();?>
+<article class="post">		
+<h2><?php the_title(); ?></h2>
+<p><?php the_content();?></p>
+</article>
+	<?php endwhile;
+
+else:
+	echo'<p>No content Found</p>';
+endif;
+
+
+get_footer();
+?>
